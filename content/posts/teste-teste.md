a299fe6dff95879fb20ec42e82b6b80446025326
Emanuel-JSA <emanuel.jsa21@gmail.com>
2024-05-14 12:45:44 -0300

    Fix dark light theme

--- content/posts/teste-teste.md
+++ content/posts/teste-teste.md
@@ -3,4 +3,4 @@ title = 'Teste Teste'
 date = 2024-04-28T13:16:49-03:00
 +++
 
-# AAAA
+aaaaa
--- docs/js/theme-toggle.js
+++ docs/js/theme-toggle.js
@@ -7,11 +7,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const toggleThemeButton = document.getElementById('theme-toggle');
     if (!toggleThemeButton) return;
 
+    // Load theme from local storage
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.body.classList.add(savedTheme);
+        const isDarkTheme = savedTheme === 'dark-theme';
+        logoDark.style.display = isDarkTheme ? 'none' : 'block';
+        logoLight.style.display = isDarkTheme ? 'block' : 'none';
+        moon.style.display = isDarkTheme ? 'none' : 'block';
+        sun.style.display = isDarkTheme ? 'block' : 'none';
+    } else {
+        const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        document.body.classList.add(isDarkTheme ? 'dark-theme' : 'light-theme');
+        logoDark.style.display = isDarkTheme ? 'none' : 'block';
+        logoLight.style.display = isDarkTheme ? 'block' : 'none';
+        moon.style.display = isDarkTheme ? 'none' : 'block';
+        sun.style.display = isDarkTheme ? 'block' : 'none';
+    }
+
     toggleThemeButton.addEventListener('click', function() {
-        document.body.classList.toggle('dark-theme');
-        logoDark.style.display = logoDark.style.display === 'none' ? 'block' : 'none';
-        logoLight.style.display = logoLight.style.display === 'none' ? 'block' : 'none';    
-        moon.style.display = moon.style.display === 'none' ? 'block' : 'none';
-        sun.style.display = sun.style.display === 'none' ? 'block' : 'none';
+        const isDarkTheme = document.body.classList.toggle('dark-theme');
+        logoDark.style.display = isDarkTheme ? 'none' : 'block';
+        logoLight.style.display = isDarkTheme ? 'block' : 'none';
+        moon.style.display = isDarkTheme ? 'none' : 'block';
+        sun.style.display = isDarkTheme ? 'block' : 'none';
+
+        // Save theme to local storage
+        localStorage.setItem('theme', isDarkTheme ? 'dark-theme' : 'light-theme');
     });
 });
